f9d3289de8a02fb0bb894af6337d05bc0de54d1e
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-08 14:15:29 +0100

    Teoria: JUnitParams testy z pobraniem danych z pliku CSV

---
+++ src/test/java/pl/sdacademy/tarr2019java4/tdd/CalculatorParametrizedJUnitParamsCSVTest.java
@@ -0,0 +1,65 @@
+package pl.sdacademy.tarr2019java4.tdd;
+
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : tarr2019java4-tdd-project
+ * @since : 08.03.2020
+ **/
+@RunWith(JUnitParamsRunner.class)
+public class CalculatorParametrizedJUnitParamsCSVTest {
+
+    @Test
+    @FileParameters("src/test/resources/CalculatorParameterCSVFileAddTest.csv")
+    public void testAdd(int numberA, int numberB, String expected) {
+
+        //given
+        Calculator calc = new CalculatorImpl();
+
+        //when
+        calc.add(numberA,numberB);
+
+        //then
+        String message = String.format("Error %d + %d != %s becouse is equal %d",
+                numberA,numberB,calc.display(),numberA+numberB);
+        assertEquals(expected, calc.display(),message);
+    }
+
+    @Test
+    @FileParameters("src/test/resources/CalculatorParameterCSVFileMultiTest.csv")
+    public void testMulti(int numberA, int numberB, String expected) {
+
+        //given
+        Calculator calc = new CalculatorImpl();
+
+        //when
+        calc.multi(numberA,numberB);
+
+        //then
+        String message = String.format("Error %d * %d != %s becouse is equal %d",
+                numberA,numberB,calc.display(),numberA*numberB);
+        assertEquals(expected, calc.display(),message);
+    }
+
+    @Test
+    @FileParameters("src/test/resources/CalculatorParameterCSVFileDivTest.csv")
+    public void testDiv(int numberA, int numberB, String expected) {
+
+        //given
+        Calculator calc = new CalculatorImpl();
+
+        //when
+        calc.div(numberA,numberB);
+
+        //then
+        String message = String.format("Error %d / %d != %s becouse is equal %d",
+                numberA,numberB,calc.display(),numberA/numberB);
+        assertEquals(expected, calc.display(),message);
+    }
+}
