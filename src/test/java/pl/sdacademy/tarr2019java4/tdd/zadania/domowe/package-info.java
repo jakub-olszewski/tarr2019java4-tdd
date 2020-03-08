e92da3fc061c7672ff5929ce64b73b11eade43f2
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-03-08 14:46:54 +0100

    Zadanie domowe

---
+++ src/test/java/pl/sdacademy/tarr2019java4/tdd/zadania/domowe/package-info.java
@@ -0,0 +1,18 @@
+package pl.sdacademy.tarr2019java4.tdd.zadania.domowe;
+
+/*
+Zadanie domowe:
+============================
+
+ * Naprawa testów poprzez implementację metod z CalculatorImpl
+   Pracujemy wykorzystując TDD czyli
+        1. Uruchamiamy wybrany test
+        2. Poprawiamy implementację
+        3. Uruchamiamy wszystkie testy, wybieramy ten który nie działa
+        4. Wracamy do punktu 1.
+        Kończymy gdy wszystko jest na zielono :)
+
+ * CalculatorParametersFieldsSubTest - utworzenie klasy parametryzujacej przez konstruktor 5 przykładów na odejmowanie
+
+
+ */
