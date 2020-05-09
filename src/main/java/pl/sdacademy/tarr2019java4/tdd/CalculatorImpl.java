7024c0a9fe44304925a2ce487b9686a317d9d5f3
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-05-09 14:07:15 +0200

    Rozwiazanie: CalculatorImpl

--- src/main/java/pl/sdacademy/tarr2019java4/tdd/CalculatorImpl.java
+++ src/main/java/pl/sdacademy/tarr2019java4/tdd/CalculatorImpl.java
@@ -7,44 +7,59 @@ package pl.sdacademy.tarr2019java4.tdd;
  **/
 public class CalculatorImpl implements Calculator {
 
+    public static String DIV_ERROR_MESSAGE = "Nie dziel przez zero";
+    //stan wyświetlacza
+    String display;
+
+    @Override
+    public void pressNumber(Object number) {
+        // dopisuje do stanu wyświetlacza
+        StringBuilder builder = new StringBuilder();
+        display = builder.append(display).append(number).toString();
+    }
+
+    //konstruktor inicjalizuje stan wyswietalacza - pusty
     public CalculatorImpl() {
         clear();
     }
 
-    private String display;
-
-    @Override
-    public void pressNumber(int number) {
-
-    }
-
     @Override
     public String display() {
+        // zwróć stan wyświetlacza
         return display;
     }
 
     @Override
     public void add(int numberA, int numberB) {
-
+        int result = numberA+numberB;
+        pressNumber(result);
     }
 
     @Override
     public void sub(int numberA, int numberB) {
-
+        pressNumber(numberA-numberB);
     }
 
     @Override
-    public void div(int numberA, int numberB) {
-
-    }
-
-    @Override
-    public void multi(int numberA, int numberB) {
+    public void div(int numberA, int numberB) throws IllegalArgumentException{
+        // implementacje zrzucanie wyjatku IllegalArgumentException
+        // podczas dzielenia przez zero
+        if(numberB==0){
+            throw new IllegalArgumentException(DIV_ERROR_MESSAGE);
+        }else
+        {
+            pressNumber(numberA / numberB);
+        }
 
     }
 
     @Override
     public void clear() {
-        display = "";
+        display="";
+    }
+
+    @Override
+    public void multi(int numberA, int numberB) {
+        pressNumber(numberA*numberB);
     }
 }
